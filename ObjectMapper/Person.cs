227c72367932187be8c86d79b459bec0db979592
Ramin-Guliyev <ra1000ishere@outlook.com>
2021-08-19 14:07:12 +0400

    RaminBeyrax

--- ObjectMapper/Person.cs
+++ ObjectMapper/Person.cs
@@ -4,6 +4,7 @@
     {
         public string  Name { get; set; }
         public int Age { get; set; }
-	public string Gender { get; set; }
+	    public string Gender { get; set; }
+         public string Ramin { get; set; }
     }
 }
